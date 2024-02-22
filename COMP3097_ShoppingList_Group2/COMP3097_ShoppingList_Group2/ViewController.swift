3f9c3e9b09f5b1d96d7e92f7247b7d4ef3a727ef
James MacAloney <121572130+JamesMaca@users.noreply.github.com>
2024-02-21 22:03:09 -0500

    created table view for list page

--- COMP3097_ShoppingList_Group2/COMP3097_ShoppingList_Group2/ViewController.swift
+++ COMP3097_ShoppingList_Group2/COMP3097_ShoppingList_Group2/ViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = item
+        
         // Do any additional setup after loading the view.
     }
 
